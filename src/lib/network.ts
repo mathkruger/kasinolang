6e2c97446830913d4987dbe9461ed31f12bf8636
Matheus Kruger <ms-kruger@hotmail.com>
2024-01-09 23:28:47 -0300

    Improving network lib

--- src/lib/network.ts
+++ src/lib/network.ts
@@ -10,28 +10,70 @@ import {
   FunctionValue,
   NULL,
   AnonymousFunctionValue,
-  StringValue,
   NumberValue,
+  ArrayValue,
+  StringValue,
 } from "../runtime/values";
-import { getPrintText } from "./std";
 
 function serve(args: RuntimeValue[], scope: Environment) {
   const port = args[0] as NumberValue;
   const fn = args[1] as FunctionValue | AnonymousFunctionValue;
 
-  Bun.serve({
+  const server = Bun.serve({
     port: port.value,
-    fetch(req) {
-      const { url, method } = req;
-      const response = runUserDefinedFunction(
-        [STRING(url), STRING(method)],
-        fn
+    async fetch(req) {
+      const { method } = req;
+      const url = new URL(req.url);
+      const searchParams = new Map<string, RuntimeValue>();
+
+      for (const s of url.searchParams) {
+        searchParams.set(s[0], STRING(s[1]));
+      }
+
+      const urlObj = OBJECT(
+        new Map<string, RuntimeValue>([
+          ["queryParams", OBJECT(searchParams)],
+          ["pathname", STRING(url.pathname)],
+        ])
       );
-      return new Response(getPrintText(response, scope, false));
+
+      const response = runUserDefinedFunction(
+        [urlObj, STRING(method)],
+        fn
+      ) as ArrayValue;
+
+      const data = response.values[0] as StringValue;
+      const type = response.values[1] as NumberValue;
+
+      switch (type.value) {
+        default:
+          return new Response(data.value, {
+            headers: {
+              "Content-Type": "text/html",
+            },
+          });
+
+        case 2:
+          return new Response(data.value);
+
+        case 3:
+          return new Response(JSON.parse(data.value));
+      }
     },
   });
 
-  return NULL();
+  return OBJECT(
+    new Map<string, RuntimeValue>([
+      [
+        "stop",
+        NATIVE_FUNCTION((_a, _v) => {
+          server.stop();
+          return NULL();
+        }),
+      ],
+      ["hostname", STRING(server.hostname)],
+    ])
+  );
 }
 
 export function network(): ObjectValue {
